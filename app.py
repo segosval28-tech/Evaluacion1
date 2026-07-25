30e1aabdb19d249cbb93a3190f8d58636396f708
segosval28-tech <seg.osval.28@gmail.com>
2026-07-25 02:36:31 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -376,7 +376,7 @@ def ejercicio_1():
     with st.form("form_movimiento", clear_on_submit=True):
         col1, col2, col3 = st.columns([1.4, 1, 1])
         concepto = col1.selectbox(
-             <h3 id=formTitle">"Concepto del movimiento"</h3>,
+             <h3 id="formTitle">Concepto del movimiento</h3>,
              [
                  "Cobro de factura comercial",
                  "Adelanto de cliente extranjero",
