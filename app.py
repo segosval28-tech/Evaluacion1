595eeeeaea7d99a9be8d204345caf30f2246d73b
segosval28-tech <seg.osval.28@gmail.com>
2026-07-25 01:50:00 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -696,9 +696,7 @@ def ejercicio_4():
 
 def main():
     inicializar_estado()
-    st.sidebar.write("Texto de prueba")
-    st.sidebar.text_input("Prueba", "Hola")
-
+   
     st.sidebar.title("Menu del proyecto")
     seccion = st.sidebar.selectbox(
         "Seleccione una seccion",
