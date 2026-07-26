d04f2f9450fcbdcd94dca6c485ad3053b9c77909
segosval28-tech <seg.osval.28@gmail.com>
2026-07-25 23:32:28 -0500

    app.py

--- app.py
+++ app.py
@@ -750,7 +750,8 @@ def ejercicio_4():
 
 def main():
     inicializar_estado()
-       st.sidebar.title("Menu del proyecto")
+
+    st.sidebar.title("Menu del proyecto")
     seccion = st.sidebar.selectbox(
         "Seleccione una seccion",
         ["Home", "Ejercicio 1", "Ejercicio 2", "Ejercicio 3", "Ejercicio 4"],
